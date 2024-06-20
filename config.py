d58a55f4b1edfc264f705f4914bade7502180cb2
satyam10869 <133392730+satyam10869@users.noreply.github.com>
2024-06-20 14:05:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("21513517"))
+API_HASH = getenv("838d3451485b95722878921877f12066")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("6798222443:AAH7RfDy9QTRJCOBR-IlEXWZ6_PvTgCJATs")
+OWNER_ID = int(getenv("5904348755"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv("mongodb+srv://sumit333:sumit333@cluster0.emqf08v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("neetumamvol2", None)
